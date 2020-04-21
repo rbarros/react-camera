0f5eec0f66bb015b9b3a14c125990a18afb7f004
Ramon Barros <ramon@lideresistemas.com.br>
2020-04-20 23:10:45 -0300

    Build project.

---
+++ precache-manifest.f125d1a80b23627f9159867d36941720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991911ddf114b78168cb41acb788b0b1",
+    "url": "/react-camera/index.html"
+  },
+  {
+    "revision": "eba98ab9526d558d6ca4",
+    "url": "/react-camera/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "57694891ef31ebbc757c",
+    "url": "/react-camera/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eba98ab9526d558d6ca4",
+    "url": "/react-camera/static/js/2.61fa23c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-camera/static/js/2.61fa23c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57694891ef31ebbc757c",
+    "url": "/react-camera/static/js/main.040b165d.chunk.js"
+  },
+  {
+    "revision": "d1817345215890174ad0",
+    "url": "/react-camera/static/js/runtime-main.701b2384.js"
+  }
+]);
